8fb61bc41095f657f86bf96e6d1cdd7a56f781ed
Mahi2897 <93644744+Mahi2897@users.noreply.github.com>
2021-11-03 14:17:18 +0530

    Add files via upload

---
+++ ConvertNegativetoPositive.java
@@ -0,0 +1,21 @@
+package week1.day1;
+
+public class ConvertNegativetoPositive {
+	
+	public static void main(String[] args) {
+		
+ int num= -40;
+  if(num<0);
+	  
+	  num= num*(-1);{
+	 System.out.println(" converted to positive :" + num);
+		
+	}
+  
+	
+	}
+}
+
+
+
+
---
+++ EvenNumbers.java
@@ -0,0 +1,16 @@
+package week1.day1;
+
+public class EvenNumbers {
+
+	public static void main(String[] args) {
+
+{
+	for (int i= 0; i <= 50; i++)
+		if(i%2==0) {
+		System.out.println(i+ "is a EvenNumbers");
+		
+}
+	}
+
+}
+}
