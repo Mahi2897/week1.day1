792e6c69b903d8204685d3fcb28743fb316185bb
Mahi2897 <93644744+Mahi2897@users.noreply.github.com>
2021-11-03 14:10:24 +0530

    Add files via upload

---
+++ TrizzFizz.java
@@ -0,0 +1,24 @@
+package week1.day1;
+
+public class TrizzFizz {
+
+	public static void main(String[] args) {
+		int Num=15;
+		if(Num%3==0 && Num%5==0) {
+			System.out.println("TRIZZ-FIZZ");
+		}
+		else if(Num%5==0) {
+			System.out.println("fIZZ");
+		}
+		else if (Num %3==0) {
+			System.out.println("TRIZZ");
+		}
+		
+		}
+		
+		// TODO Auto-generated method stub
+
+	}
+
+
+
